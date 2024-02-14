b002d2bb8764e919a5b4466ce2cba5e0b3b8ccfa
alberic <alberic.briot@osiris-team.fr>
2024-02-14 09:42:47 +0100

    SECU : rm db/connection.yml

--- around/prod_backup.sh
+++ around/prod_backup.sh
@@ -4,7 +4,9 @@ RSYNC_CMD='rsync -avpzh kaja9241@openproduct.fr'
 
 $RSYNC_CMD:~/public_html/wiki/LocalSettings.php ../private/
 $RSYNC_CMD:public_html/ --files-from=backup_file.lst ../public/
+# Particular files
 $RSYNC_CMD:~/public_html/unsubscribe.php var.www.openproduct.wiki.unsubscribe.php
+$RSYNC_CMD:~/public_html/db/connection.yml ../db/
 echo "Wiki DB"
 ssh kaja9241@openproduct.fr "mysqldump -ukaja9241_wiki -p kaja9241_openproduct_wiki" > openproduct_wiki.dump.sql
 echo "DB"
