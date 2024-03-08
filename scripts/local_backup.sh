6a89abd88e116a2593607a8c897845ef58453112
alberic <alberic.briot@osiris-team.fr>
2024-03-08 23:09:40 +0100

    local_bakup.sh : Separate and select tables to dump etc.nginx.sites-available : Try add PixelHumain map.html : temporary disable "produceFilter" : Time to feel in DB

--- scripts/local_backup.sh
+++ scripts/local_backup.sh
@@ -8,5 +8,9 @@ echo "Local en backup"
 # cp /var/www/openproduct/wiki/unsubscribe.php ../around/var.www.openproduct.wiki.unsubscribe.php
 cp /etc/nginx/sites-available/openproduct ../around/etc.nginx.sites-available.openproduct
 # mysqldump -uroot -posiris openproduct_wiki > $DB_BACKUP_PATH/openproduct_wiki.dump.sql
-mysqldump -uroot -posiris openproduct > $DB_BACKUP_PATH/openproduct.dump.sql
+for table in producer produce product_link; do
+	echo "Table:$table"
+	mysqldump -uroot -posiris --no-data openproduct $table > $DB_BACKUP_PATH/openproduct.$table.schema.sql
+	mysqldump -uroot -posiris --no-create-info --complete-insert openproduct $table > $DB_BACKUP_PATH/openproduct.$table.data.sql
+done
 
